c60d96809a61536d091bae485ef3e9fbcb5eccb9
skmutai6 <skmutai6@gmail.com>
2024-03-22 14:49:35 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks() {
+    let block43;
+    let block42;
+    return 1;
+}
+
+function distanceFromHqInBlocks(){
+    let block50;
+    let block42;
+    return 8;
+}
+
+function distanceFromHqInBlocks(){
+    let block42;
+    let block34;
+    return 8;
+}
+
+function distanceFromHqInFeet(){
+    let block = x;
+    let x = 264;
+    distanceFromHqInFeet = x * (43 - 42);
+    {
+        console.log(x);
+    }
+
+}
+
+
+function distanceTravelledInFeet(){
+    let distanceTravelledInFeet = x;
+    let x = ((43-48) * 264);
+    return 1320;
+}
+
+// function distanceTravelledInFeet(){
+//     let distanceTravelledInFeet = x;
+//     let x = ((60 - 50) * 264);
+//     return 2640;
+// }
+
